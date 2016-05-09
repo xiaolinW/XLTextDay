417432216c27e31ccebc1ce312409b6171ed7bdc
Mac <mac@macdemac-mini.local>
2016-05-09 20:56:36 +0800

    Initial Commit

---
+++ csadsa/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  csadsa
+//
+//  Created by Mac on 16/5/9.
+//  Copyright © 2016年 Mac. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
